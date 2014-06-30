b2b0bc8809aaeddbf0fb1c3572a15325c90fae24
David Liu <oinkmoobaa@gmail.com>
2014-06-30 14:57:21 -0700

    changes

--- do_stuff.py
+++ do_stuff.py
@@ -1,13 +1,11 @@
 import urllib2
 import json
 
-user = 'gausanka'
+user = raw_input()
+#user = 'gausanka'
 
 x = urllib2.urlopen("https://api.github.com/users/" + user).read()
 repos = urllib2.urlopen("https://api.github.com/users/" + user + "/repos").read()
-
-
-print x
 
 x = json.loads(x)
 repos = json.loads(repos)
@@ -15,6 +13,7 @@ repos = json.loads(repos)
 a = {}
 a['user'] = user
 a['avatar'] = x['avatar_url']
+a['followers'] = int(x['followers'])
 a['repos'] = []
 for i in repos:
     z = {}
@@ -23,9 +22,10 @@ for i in repos:
     z['url'] = i['url']
     z['size'] = int(i['size'])
     z['watchers_count'] = int(i['watchers_count'])
+    z['forks'] = int(i['forks'])
     a['repos'].append(z)
 a['total_repo_size'] = sum(z['size'] for z in a['repos'])
-a['total_watchers'] = sum(z['watchers_count'] for z in a['repos'])
+a['total_repo_watchers'] = sum(z['watchers_count'] for z in a['repos'])
 
 print json.dumps(a, indent = 4)
    
